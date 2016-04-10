ba1e79cc5096df1b6a80e1331cac8eafa586776d
Omar Hefnawi <omarhefnawi@gmail.com>
2016-04-09 17:24:16 -0700

    FINAL BASH, OMAR SMASH

--- deployment.bash
+++ deployment.bash
@@ -2,13 +2,14 @@
 #This machine will be a tracker, logger and seeder
 
 # Assuming I'm remote.ugrad.cs.ubc.ca 
-servers=( lulu.ugrad.cs.ubc.ca annacis.ugrad.cs.ubc.ca bowen.ugrad.cs.ubc.ca thetis.ugrad.cs.ubc.ca deas.ugrad.cs.ubc.ca )
+#servers=( lulu.ugrad.cs.ubc.ca annacis.ugrad.cs.ubc.ca bowen.ugrad.cs.ubc.ca thetis.ugrad.cs.ubc.ca deas.ugrad.cs.ubc.ca )
+servers=( annacis.ugrad.cs.ubc.ca )
 trackerport=10000
 loggerport=10001
 
 #read file
 echo "Enter file to seed/track/whatever"
-file="fireflies.mp3"
+file="grimgar.mkv"
 #read file
 
 #kills all running taipeis, Shit way of doing it 
@@ -16,25 +17,17 @@ file="fireflies.mp3"
 killall CPSC416Taipei
 killall logger
 
-thisip=$(ip addr show em1 | grep inet | awk '{print $2}' | sed s_\/..__g )
+thisip=$(ip addr show em1 | grep inet | awk '{print $2}' | sed s_\/..__g | head -1 )
+echo Got this ip: $thisip
 
 ./CPSC416Taipei -createTracker $thisip:$trackerport -createTorrent $file > test.torrent
 ./CPSC416Taipei -createTracker $thisip:$trackerport test.torrent &
 ./logger $thisip $loggerport &
 
+./CPSC416Taipei -port 0 -loggerAddress $thisip:$loggerport ~/cs416/test/test.torrent &
+
 for server in ${servers[@]}
 do
   mkdir -p ~/cs416/test/download/$server
-  ssh -t t8e8@$server " 
-  cd cs416/test/download/$server;
-  nohup ../.././CPSC416Taipei ../../test.torrent &
-  " 
+  ssh t8e8@$server "~/cs416/test/CPSC416Taipei -port 0 -fileDir ~/cs416/test/download/$server -loggerAddress $thisip:$loggerport ~/cs416/test/test.torrent &"
 done
-
-#for server in ${servers[@]}
-#do
-#  ssh -t t8e8@$server "
-#  cd cs416/test/download/
-#done
-
-# ssh -t t8e8@${servers[$i]} "cd cs416/test/download/$i; ../.././CPSC416Taipei ../../test.torrent"
